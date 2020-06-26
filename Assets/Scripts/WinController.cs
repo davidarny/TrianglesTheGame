af4bacd45f12c3be4b59ff36ecf8cea5627e1098
David Arutiunian <arutunjan666@mail.com>
2020-06-26 13:52:28 +0300

    🐛 Fix "Level Completed!" overflow
    💄 Change "Perfect!" and "Level Completed!" colors

--- Assets/Scripts/WinController.cs
+++ Assets/Scripts/WinController.cs
@@ -25,11 +25,11 @@ public class WinController : MonoBehaviour
         if (GameStore.instance.IsEndOfLevel())
         {
             win.text = TEXT_LEVEL_COMPLETED;
-            win.color = new Color32(255, 255, 255, 255);
+            win.color = new Color32(138, 201, 38, 255);
         }
         else
         {
-            win.color = new Color32(247, 168, 184, 255);
+            win.color = new Color32(255, 202, 58, 255);
             win.text = TEXT_PERFECT;
         }
 
